1a3cc89c71f92b0d4d84ef3d08635018a83f995f
Evan Quan <evanlquan@gmail.com>
2018-03-11 14:56:22 -0600

    Tests now consider different line separators depending on OS

--- src/test/TestSuite.java
+++ src/test/TestSuite.java
@@ -17,12 +17,21 @@ import org.junit.runners.Suite;
  * Runs all test classes
  *
  * @author Evan Quan
- * @since March 9, 2018
+ * @since March 11, 2018
  *
  */
 public class TestSuite {
-	// Base directory is the test package directory
+	/**
+	 * Base directory is the test package directory
+	 */
 	public static String BASEDIR = new File("").getAbsolutePath().concat("/src/test/");
-	// test.testPackage package directory is contained in test package directory
+	/**
+	 * test.testPackage package directory is contained in test package directory
+	 */
 	public static String JAVA_FILE_READER_TEST_DIR = BASEDIR.concat("javaFileReaderTestPackage/");
+	/**
+	 * Line separator changes depending on operating system. JUnitTests dealing with
+	 * file contents should consider this.
+	 */
+	public static String lineSeparator = System.getProperty("line.separator");
 }
